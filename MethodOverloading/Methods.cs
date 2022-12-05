b828af636d3f4d1b5d77b663a3113aec5a45a2b5
Michael Maldonado <MichaelFrankMaldonado@gmail.com>
2022-12-04 21:48:02 -0700

    Exercise

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace MethodOverloading
+{
+	public class Methods
+	{
+		public static int Add(int num1, int num2)
+		{
+			return num1 + num2;
+		}
+
+
+		public static decimal Add(decimal num1, decimal num2)
+		{
+			return num1 + num2;
+		}
+
+
+		public static string Add(int num1, int num2, bool input1)
+		{
+			int sum = num1 + num2;
+
+
+			if (input1)
+			{
+				if (sum == 1)
+				{
+					return "1 dollar";
+				}
+
+				else
+				{
+					return $"{sum} dollars";
+				}
+			}
+			else
+			{
+				return "You have an invalid entry!";
+			}
+
+			//else
+			//{
+			//	return "Please enter a valid number";
+			//}
+		}
+		
+
+
+
+	}
+
+}
+//Now create another overload of the Add method that returns a string and accepts 3 parameters: 2 integers and 1 boolean
+//If the boolean parameter is equal to true, the Add method will return the sum of the 2 numbers plus the word “dollars” at the end of the string.
+//You must also account for the appropriate grammar of the string - so it can’t be “1 dollars”
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace MethodOverloading
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using System.Net.NetworkInformation;
+
+using MethodOverloading;
+
+
+        int answer1 = Methods.Add(5,5);
+        Console.WriteLine (answer1);
+
+        decimal answer2 = Methods.Add(10.5m, 9.5m);
+        Console.WriteLine(answer2);
+
+        string answer3 = Methods.Add(1, 10, true);
+        Console.WriteLine(answer3);
+
